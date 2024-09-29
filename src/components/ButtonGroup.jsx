24a90cab143e390ae9cafcb9192b0cc13310b85a
Ahmed Khadawardi <ahes0001@student.monash.edu>
2024-09-29 16:27:17 +1000

    ItemList component logic set up with useState and initialItems

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,10 +1,6 @@
+import { secondaryButtons } from "../lib/constants";
 import Button from "./Button";
-const secondaryButtons = [
-    "Mark all as complete",
-    "Mark all as incomplete",
-    "Reset to initial",
-    "Remove all items",
-  ];
+
 
 export default function ButtonGroup() {
 
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,7 +1,24 @@
+
+import { initialItems } from "../lib/constants";
+import { useState } from "react";
+
 export default function ItemList() {
+  const [items, setItems] = useState(initialItems);
+
   return (
-    <div>
-      ItemList
-    </div>
-  )
+    <ul>
+      {items.map((item) => <Item key = {item.id} item = {item}/>)}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <ul>
+      <li className="item">
+         <label ><input type="checkbox"/> {item.name}</label>
+         <button>❌</button>
+      </li>
+    </ul>
+  );
 }
