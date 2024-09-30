55ef11683afe7d8859f31e1ab020cfd4f611df80
Ahmed Khadawardi <ahes0001@student.monash.edu>
2024-10-01 06:58:31 +1000

    completed sorting functinalities

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 import EmptyView from "./EmptyView";
+import Select from "react-select";
+
+const sortingOptions = [
+  { value: "default", label: "Sort by default" },
+  { value: "packed", label: "Sort by packed" },
+  { value: "unpacked", label: "Sort by unpacked" },
+];
 
 export default function ItemList({ items, handleRemoveItem, handleToggleItem }) {
   
+  const [sortBy, setSortBy] = useState("default");
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === "packed") {
+      return b.packed - a.packed;
+    } else if (sortBy === "unpacked") {
+      return a.packed - b.packed;
+    }
+    return;
+  });
 
   return (
     <ul className="item-list">
       {
         items.length === 0? <EmptyView/>: null
       }
+      {
+        items.length > 0 && <section className="sorting">
+          <Select onChange= {option => setSortBy(option.value)} defaultValue ={sortingOptions[0]} options={sortingOptions}/>
+        </section>
+      }
 
-      {items.map((item) => <Item key = {item.id} handleRemoveItem = {handleRemoveItem} handleToggleItem = {handleToggleItem} item = {item}/>)}
+      {sortedItems.map((item) => <Item key = {item.id} handleRemoveItem = {handleRemoveItem} handleToggleItem = {handleToggleItem} item = {item}/>)}
     </ul>
   );
 }
