64ed885029f5a02c4f04ddb5fa4bb9f72f005b7d
Ahmed Khadawardi <ahes0001@student.monash.edu>
2024-10-01 13:25:51 +1000

    optimisation: useMemo

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import EmptyView from "./EmptyView";
 import Select from "react-select";
 
@@ -8,31 +8,47 @@ const sortingOptions = [
   { value: "unpacked", label: "Sort by unpacked" },
 ];
 
-export default function ItemList({ items, handleRemoveItem, handleToggleItem }) {
-  
+export default function ItemList({
+  items,
+  handleRemoveItem,
+  handleToggleItem,
+}) {
   const [sortBy, setSortBy] = useState("default");
 
-  const sortedItems = [...items].sort((a, b) => {
-    if (sortBy === "packed") {
-      return b.packed - a.packed;
-    } else if (sortBy === "unpacked") {
-      return a.packed - b.packed;
-    }
-    return;
-  });
+  const sortedItems = useMemo(
+    () =>
+      [...items].sort((a, b) => {
+        if (sortBy === "packed") {
+          return b.packed - a.packed;
+        } else if (sortBy === "unpacked") {
+          return a.packed - b.packed;
+        }
+        return;
+      }),
+    [items, sortBy]
+  );
 
   return (
     <ul className="item-list">
-      {
-        items.length === 0? <EmptyView/>: null
-      }
-      {
-        items.length > 0 && <section className="sorting">
-          <Select onChange= {option => setSortBy(option.value)} defaultValue ={sortingOptions[0]} options={sortingOptions}/>
+      {items.length === 0 ? <EmptyView /> : null}
+      {items.length > 0 && (
+        <section className="sorting">
+          <Select
+            onChange={(option) => setSortBy(option.value)}
+            defaultValue={sortingOptions[0]}
+            options={sortingOptions}
+          />
         </section>
-      }
+      )}
 
-      {sortedItems.map((item) => <Item key = {item.id} handleRemoveItem = {handleRemoveItem} handleToggleItem = {handleToggleItem} item = {item}/>)}
+      {sortedItems.map((item) => (
+        <Item
+          key={item.id}
+          handleRemoveItem={handleRemoveItem}
+          handleToggleItem={handleToggleItem}
+          item={item}
+        />
+      ))}
     </ul>
   );
 }
@@ -41,8 +57,15 @@ function Item({ item, handleRemoveItem, handleToggleItem }) {
   return (
     <ul>
       <li className="item">
-         <label ><input onChange = {()=>handleToggleItem(item.id)} checked={item.packed} type="checkbox"/> {item.name}</label>
-         <button onClick={()=>handleRemoveItem(item.id)}>❌</button>
+        <label>
+          <input
+            onChange={() => handleToggleItem(item.id)}
+            checked={item.packed}
+            type="checkbox"
+          />{" "}
+          {item.name}
+        </label>
+        <button onClick={() => handleRemoveItem(item.id)}>❌</button>
       </li>
     </ul>
   );
